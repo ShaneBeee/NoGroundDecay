a6d463a9557bcb0948115602403778f88e930710
BuildTools <shanebolenback@me.com>
2018-10-04 11:02:03 -0700

    Creating 1.12.2 version

--- src/tk/shanebee/noleafdecay/Event.java
+++ src/tk/shanebee/noleafdecay/Event.java
@@ -3,7 +3,6 @@ package tk.shanebee.noleafdecay;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.type.Leaves;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.LeavesDecayEvent;
@@ -15,23 +14,14 @@ public class Event implements Listener {
         Block below = event.getBlock().getRelative(BlockFace.DOWN);
         Block twobelow = below.getRelative(BlockFace.DOWN);
 
-        if (below.getType() == Material.GRASS_BLOCK || below.getType() == Material.DIRT || below.getType() == Material.PODZOL || below.getType() == Material.COARSE_DIRT) {
+        if (below.getType() == Material.GRASS || below.getType() == Material.DIRT) {
 
             event.setCancelled(true);
-            Leaves leaf = (Leaves) event.getBlock().getState().getBlockData();
-            leaf.setPersistent(true);
-            event.getBlock().setBlockData(leaf);
 
-        }
-        else if (twobelow.getType() == Material.GRASS_BLOCK || twobelow.getType() == Material.DIRT || twobelow.getType() == Material.PODZOL || twobelow.getType() == Material.COARSE_DIRT) {
+        } else if (twobelow.getType() == Material.GRASS || twobelow.getType() == Material.DIRT) {
 
             event.setCancelled(true);
-            Leaves leaf = (Leaves) event.getBlock().getState().getBlockData();
-            leaf.setPersistent(true);
-            event.getBlock().setBlockData(leaf);
-
         }
 
     }
-
 }
