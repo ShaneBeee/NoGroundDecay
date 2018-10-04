3febe03c8d95a044a963032844546b819f3e4a0e
BuildTools <shanebolenback@me.com>
2018-10-04 14:08:32 -0700

    Fancy new loading message, with some color and version

--- src/tk/shanebee/noleafdecay/Main.java
+++ src/tk/shanebee/noleafdecay/Main.java
@@ -1,13 +1,19 @@
 package tk.shanebee.noleafdecay;
 
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-    @Override
     public void onEnable() {
 
-        getLogger().info("Loaded successfully!");
+        PluginDescriptionFile pdfFile = getDescription();
+
+        String pre = ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + pdfFile.getName() + ChatColor.DARK_GRAY + "]";
+        String ver = ChatColor.GRAY + "v" + pdfFile.getVersion() + ChatColor.RESET;
+
+        getServer().getConsoleSender().sendMessage(pre + ChatColor.GREEN + " Successfully loaded " + ver);
         getServer().getPluginManager().registerEvents(new Event(), this);
     }
 
